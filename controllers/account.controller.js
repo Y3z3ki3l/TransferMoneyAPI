689217e3e1b1083846c0b85df5ee0a6d22cc77ae
Franz Ezequiel Muñoz Gutierrez <franzmunoz@gmail.com>
2020-05-11 10:44:18 -0500

    Add controllers for each functionality

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -5,3 +5,53 @@ const Account = require('../dao/account.dao');
 exports.testAccountCtrlr = function (req, res) {
     res.send('Greetings from the account controller TEST!');
 };
+
+exports.create = function (req, res, next) {
+    
+    // Validate request
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        return res.status(400).send({
+            message: "Account info can not be empty"
+        });
+    }
+
+    // Create an Account object from date model
+    const account = new Account({
+        accountNumber: req.body.accountNumber,
+        balance: req.body.balance,
+        owner: req.body.owner
+    });
+
+    // Save Account in the database
+    account.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
+
+}
+
+exports.getAccountBalance = function(req, res, next) {
+
+    console.log("Request params: " + req.params.accountNumber);
+
+    Account.getByAccountNum({accountNumber: req.params.accountNumber}, 
+        function(err, account) {
+            if(err) {
+                res.json({
+                    error: err
+                })
+            }
+
+            var data_response = account
+            console.log("Account obtained: " + account);
+            console.log("Account balance: " + account[0].balance);
+
+            res.json({
+                balance: account
+            })
+        })
+}
--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -5,3 +5,123 @@ const Account = require('../dao/account.dao');
 exports.testTransactionCtrlr = function (req, res) {
     res.send('Greetings from the transaction controller TEST!');
 };
+
+exports.createTransaction = function (req, res, next) {
+
+    Account.getByAccountNum({accountNumber: req.body.fromAccount}, 
+        function(err, account) {
+            if(err) {
+                res.json({
+                    error: err
+                })
+            }
+
+            console.log("Request body -> amount: " + req.body.amount);
+
+            // var data_response = account
+            // var reqParams = req;
+
+            console.log("Source Account: " + account);
+            console.log("Source Account Balance: " + account[0].balance);
+            // console.log("Amount to extract from Source Account: " + reqParams.amount);
+
+            if ( (account[0].balance - req.body.amount) >= -500.00 ) {
+                
+                // let's update source account and destination account 
+                // balance
+                // UPDATE SOURCE ACCOUNT BALANCE
+                var sourceBalance = {
+                    balance: account[0].balance - req.body.amount
+                }
+
+                Account.update({accountNumber: req.body.fromAccount}, sourceBalance,
+                    
+                    function(err, sourceAccount){
+
+                        if (err) {
+                            res.json({
+                                error: err
+                            })
+                        }
+                })
+
+                // UPDATE DESTINATION ACCOUNT BALANCE
+                Account.getByAccountNum({accountNumber: req.body.toAccount}, 
+                    function(err, account) {
+                        if(err) {
+                            res.json({
+                                error: err
+                            })
+                        }
+
+                    var destBalance = {
+                        balance: account[0].balance + req.body.amount
+                    }
+        
+                    Account.update({accountNumber: req.body.toAccount}, destBalance,
+                    
+                        function(err, destAccount){
+        
+                            if (err) {
+                                res.json({
+                                    error: err
+                                })
+                            }
+                        })
+                })
+
+                // Now let's create the transaction
+                var transaction = {
+                    fromAccount: req.body.fromAccount,
+                    toAccount: req.body.toAccount,
+                    amount: req.body.amount
+                }
+
+                Transaction.create(transaction, function(err, createdTransaction){
+
+                    if (err) {
+                        res.json({
+                            error: err
+                        })
+                    }
+
+                    console.log("What res is? -> " + createdTransaction);
+
+                    /* 
+                    res.json({
+                        message01: "Source Account balance successfully updated",
+                        message02: "Destination Account balance successfully updated",
+                        message03: "Transaction successfully created"
+                    })
+                    */
+ 
+                    res.json({
+                        message: "Transaction successfully created"
+                    })
+                })
+
+            } else {
+                res.json({
+                    message: "Transaction didn't proceed," 
+                    + " Source Account doesn't have enough balance for transfer money"
+                })
+            }
+        })
+
+}
+
+exports.geTransactions = function (req, res, next) {
+
+    Transaction.get({}, function(err, transactions){
+
+        if (err) {
+            res.json({
+                error: err
+            })
+        }
+        res.json({
+            transactions: transactions
+        })
+    })
+
+}
