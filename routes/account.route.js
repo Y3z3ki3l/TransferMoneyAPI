8729790785b01aeb62e888ecabb50591574f8795
Franz Ezequiel Muñoz Gutierrez <franzmunoz@gmail.com>
2020-05-11 10:48:58 -0500

    Add autogeneration docs with swagger

--- routes/account.route.js
+++ routes/account.route.js
@@ -4,10 +4,69 @@ const router = express.Router();
 // Require the related controller
 const account = require('../controllers/account.controller');
 
+ /**
+ * @swagger
+ * /api/test/account:
+ *  get:
+ *    description: Use to account controller simple test.
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/test/account', account.testAccountCtrlr);
 
+/**
+ * @swagger
+ *  /api/account/create:
+ *   post:
+ *      description: Use to create a new account
+ *      consumes:
+ *          - application/json
+ *      parameters:
+ *          - in: body
+ *            name: account
+ *            description: Account to be created.
+ *            schema:
+ *              type: object
+ *              required:
+ *                  - accountNumber
+ *                  - balance
+ *                  - owner
+ *              properties:
+ *                  accountNumber:
+ *                      type: int64
+ *                      description: Account's number
+ *                  balance:
+ *                      type: double
+ *                      description: Account's balance
+ *                  owner:
+ *                      type: int64
+ *                      description: Account's owner
+ *                  createdAt:
+ *                      type: Date
+ *                      description: Account's created date
+ *      responses:
+ *          200:
+ *            description: New account successfully created.
+ */
 router.post('/account/create', account.create);
 
+/**
+ * @swagger
+ *  /api/account/{accountNumber}/getBalance:
+ *   get:
+ *      parameters:
+ *          - in: path
+ *            name: accountNumber
+ *            required: true
+ *            schema:
+ *              type: integer
+ *              minimum: 1
+ *            description: The Account's number
+ *      responses:
+ *          200:
+ *            description: OK
+ */
 router.get('/account/:accountNumber/getBalance', account.getAccountBalance);
 
 module.exports = router;
--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -5,10 +5,61 @@ const router = express.Router();
 const transaction = require('../controllers/transaction.controller');
 
 // A simple test url to check that all of our files are communicating correctly.
+/**
+ * @swagger
+ * /api/test/transaction:
+ *  get:
+ *    description: Use to transaction controller simple test
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/test/transaction', transaction.testTransactionCtrlr);
 
+/**
+ * @swagger
+ *  /api/transaction/create:
+ *   post:
+ *      description: Use to create a new transaction
+ *      consumes:
+ *          - application/json
+ *      parameters:
+ *          - in: body
+ *            name: transaction
+ *            description: Transaction to be created.
+ *            schema:
+ *              type: object
+ *              required:
+ *                  - fromAccount
+ *                  - toAccount
+ *                  - amount
+ *              properties:
+ *                  fromAccount:
+ *                      type: int64
+ *                      description: Source Account
+ *                  toAccount:
+ *                      type: int64
+ *                      description: Destination Account
+ *                  amount:
+ *                      type: double
+ *                      description: Amount to be transfered
+ *                  sentAt:
+ *                      type: Date
+ *                      description: Transaction's created date
+ *      responses:
+ *          200:
+ *            description: New transaction successfully created.
+ */
 router.post('/transaction/create', transaction.createTransaction);
 
+/**
+ * @swagger
+ *  /api/transaction/getAll:
+ *   get:
+ *      responses:
+ *          200:
+ *            description: All Transactions
+ */
 router.get('/transaction/getAll', transaction.geTransactions);
 
 module.exports = router;
